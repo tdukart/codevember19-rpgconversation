6a112249904bf004f65471004bba94a50075012c
Todd Dukart <todd.dukart@globe.com>
2019-11-07 10:41:28 -0500

    Add Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,38 @@
-import * as React from "react";
-import { render } from "react-dom";
-import styled from "@emotion/styled";
+import * as React from 'react';
+import { render } from 'react-dom';
+import styled from '@emotion/styled';
 
-import "./styles.css";
+import './styles.css';
 
 const ConversationContainer = styled.div({
-  position: "absolute",
+  position: 'absolute',
   bottom: 0,
   left: 0,
   right: 0,
-  display: "flex"
+  display: 'flex'
 });
 
 const ConservationDialog = styled.div({
-  border: "1px solid #000",
-  borderRadius: "3em",
-  minWidth: "80%",
+  border: '1px solid #000',
+  borderRadius: '3em',
+  minWidth: '80%',
   flex: 1,
-  height: "250px",
-  margin: "1em",
-  paddingTop: "1.325em"
+  height: '250px',
+  margin: '1em',
+  paddingTop: '1.325em'
 });
 
-type SpeakerProps = { speakerPosition?: "left" | "right" };
+type SpeakerProps = { speakerPosition?: 'left' | 'right' };
 
-const Speaker = styled.div<SpeakerProps>(({ speakerPosition = "left" }) => ({
-  border: "1px solid #000",
-  borderRadius: "2em",
-  width: "15em",
-  position: "absolute",
-  [speakerPosition]: "10%",
-  height: "2em",
-  background: "#fff",
-  top: "-.25em"
+const Speaker = styled.div<SpeakerProps>(({ speakerPosition = 'left' }) => ({
+  border: '1px solid #000',
+  borderRadius: '2em',
+  width: '15em',
+  position: 'absolute',
+  [speakerPosition]: '10%',
+  height: '2em',
+  background: '#fff',
+  top: '-.25em'
 }));
 
 function App() {
@@ -46,5 +46,5 @@ function App() {
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 render(<App />, rootElement);
