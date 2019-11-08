699211ebd5ee6489673fa596aefb9c0e0d84ee64
Todd Dukart <786421+tdukart@users.noreply.github.com>
2019-11-08 18:54:38 -0500

    Convert Conversation to component

--- src/index.tsx
+++ src/index.tsx
@@ -2,53 +2,15 @@
 
 import * as React from 'react';
 import { render } from 'react-dom';
-import styled from '@emotion/styled';
+import Conversation from './components/Conversation';
 
 import './styles.css';
 
-/*
-Fonts:
-font-family: 'Big Shoulders Display', cursive;
-font-family: 'Big Shoulders Text', cursive;
- */
-
-const ConversationContainer = styled.div({
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  right: 0,
-  display: 'flex',
-});
-
-const ConservationDialog = styled.div({
-  border: '1px solid #000',
-  borderRadius: '3em',
-  minWidth: '80%',
-  flex: 1,
-  height: '250px',
-  margin: '1em',
-  paddingTop: '1.325em',
-});
-
-type SpeakerProps = { speakerPosition?: 'left' | 'right' };
-
-const Speaker = styled.div<SpeakerProps>(({ speakerPosition = 'left' }) => ({
-  border: '1px solid #000',
-  borderRadius: '2em',
-  width: '15em',
-  position: 'absolute',
-  [speakerPosition]: '10%',
-  height: '2em',
-  background: '#fff',
-  top: '-.25em',
-}));
-
 const App = (): React.ReactElement => (
   <div className="App">
-    <ConversationContainer>
-      <Speaker>Me</Speaker>
-      <ConservationDialog>hello world</ConservationDialog>
-    </ConversationContainer>
+    <Conversation speaker="Somebody" speakerPosition="left">
+      Hello world!
+    </Conversation>
   </div>
 );
 
